6845431b31250e4ff0468e38d3432af2f7498315
Qwaieare <gb-grand@mail.ru>
2022-12-05 00:14:08 +0300

    coursePaper20221204

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        String [] [] book = {
+                {"’Мертвые", "души’", "Николай", "Васильевич", "Гоголь", "1841"},
+        {"’Пиковая", "дама’", "Александр", "Сергеевич", "Пушкин", "1833"}
+                 };
+
+        for (int i = 0; i < book.length; i++) {
+            for (int j = 0; j < book[i].length; j++)
+                System.out.print(book[i][j] + " ");
+            System.out.println();
+        }
     }
 }
+
+
